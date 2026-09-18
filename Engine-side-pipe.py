6b8cf4d04523f43bd55bab69fa17946fdb60c4e6
ThePixelAritst <ondrej@zapadlo.name>
2026-09-18 17:14:38 +0200

    Untested handshake sequence

--- Engine-side-pipe.py
+++ Engine-side-pipe.py
@@ -1,69 +1,114 @@
 from multiprocessing import Process as mppr
 import multiprocessing.connection as mpcon
 
+
+# VERSION --- V1.0.0
+
 class Arbiter_communication:
     # IMPORTANT NOTE: YOUR ENGINE WILL NOT EVEN START IF YOU TOUCH ANYTHING IN HERE!
 
     # Public-facing functions, which you can (and need) to use in order to communicate with the Arbiter
 
     def receive_arbiter_instruction(self):
+        if not self.pipe_open:
+            raise RuntimeError("Cannot receive data through closed pipe")
         request = self.pipe.recv() # blocking instruction - waits for data from Arbiter
-        if self._check_receive_validity(request):
-            return request
-        else:
-            raise ValueError("Invalid request received, resending")
+        return self._handle_received(request)
             
 
     def send_to_arbiter(self,data_to_send): #sends the inputed data packet to Arbiter through pipe
+        if not self.pipe_open:
+            raise RuntimeError("Cannot send data through closed pipe")
         if not data_to_send:
             raise ValueError("Empty packet cannot be sent to Arbiter!")
         self.pipe.send(data_to_send)
+
 
     # Interal back-end functions and workings, you likely wont need these :D
 
     def __init__(self,pipe_conn: mpcon.Connection):
             self.done_handshake = False
             self.pipe = pipe_conn
-            self._define_translation_dictionary()     
-            self.send_to_arbiter(("INITIAL","READY"))
+            self.pipe_open = True
+            self._define_translation_dictionary()
+            self.send_to_arbiter(("ARBITER","READY",()))
+            
 
     def _handshake(self):
-        while not self.done_handshake:
-            instruction = self.receive_arbiter_instruction()
+        if not self.receive_arbiter_instruction():
+            print("Engine has failed communication handshake")
+            self.send_to_arbiter(("ARBITER","FATAL",()))
+            return
 
+        while not self.done_handshake:
+            request, valid = self.receive_arbiter_instruction()
+            if valid:
+                self.send_to_arbiter(("ARBITER","FUNCTION-VALID",(request)))
+            else:
+                self.send_to_arbiter(("ARBITER","fUNCTION-INVALID",(request)))
+                
 
     def _define_translation_dictionary(self):
         self.arbiter_translation = {
-            "HANDSHAKE":(self._handshake(),0)
+            "HANDSHAKE_START":(self._handshake,0),
+            "PIPE_CLOSE": (self.__close_pipe,0),
+            "HANDSHAKE_OK" : (self.__confirm_handshape,0)
             }
         
-        self.translate_receive_type = {
+        self.translate_public_function = {
             "MOVE": int,
             "PONDER" : int,
-            "SETTINGS" : int,
+            "SETTINGS" : int
             }
 
 
     def _check_receive_validity(self,input_data):
-        if input_data is type(None):
-            self.send_to_arbiter(("ARBITER","PIPE_CLOSED"))
-            self.pipe.close()
-
-        if not self.done_handshake:
-            return True
-        elif input_data[0] is "ARBITER":
-            argument_length = len(input_data)-2
-            requested_function = self.arbiter_translation.get(input_data[1],KeyError)
+        arbiter_method = False  
+        if input_data[0] is "ARBITER":
+            dictionary_reply = self.arbiter_translation.get(input_data[1],KeyError)
+            if dictionary_reply is not KeyError:
+                request_arguments = len(input_data[2])
+                arbiter_method = True       
         else:
-            requested_function = self.translate_receive_type.get(input_data[0],KeyError)
-            argument_length = len(input_data)-1
+            dictionary_reply = self.translate_public_function.get(input_data[0],KeyError)
+            if dictionary_reply is not KeyError:
+                request_arguments = len(input_data[1])
 
-        if requested_function is not KeyError and argument_length == requested_function[1]:
-            return True
+
+        if requested_function is not KeyError:
+            requested_function = dictionary_reply[0]
+            function_arguments = dictionary_reply [1]
+            if arbiter_method and type(requested_function) is not function:
+                arbiter_method = False
+            if request_arguments == function_arguments:
+                return True, arbiter_method
+
+        return False
+
+
+    def _handle_received(self,received_data: tuple):
+        valid, for_arbiter_execution = self._check_receive_validity()
+
+        if for_arbiter_execution():
+            self.arbiter_translation[received_data[1]](received_data[2])
+        elif valid and self.done_handshake:
+            return received_data 
+        elif not self.done_handshake:
+            return received_data, valid
         else:
-            self.send_to_arbiter(("ARBITER","INVALID_REQUEST",input_data))
-            return False
+            raise ValueError("Invalid request received")
 
+
+    def __confirm_handshape(self):
+        self.done_handshake = True
+
+
+    def __close_pipe(self):
+        self.send_to_arbiter(("ARBITER","PIPE_CLOSED",()))
+        self.pipe.close()
+
+
+        
 
 
 
