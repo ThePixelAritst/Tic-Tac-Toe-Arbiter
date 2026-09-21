6048d18aa4448d1fa337006fb75500002d079b35
Pixel <ondrej@zapadlo.name>
2026-09-21 16:41:45 +0200

    Engine base 1st rough

--- Engine_base/Engine_base.py
+++ Engine_base/Engine_base.py
@@ -1,2 +1,25 @@
-import Engine_base.Arbiter_communications as comms
+import Arbiter_communications
 
+
+class Main_Engine:
+    def __init__(self,pipe_conn):
+        self.comms = Arbiter_communications.Arbiter_communication(pipe_conn)
+
+        self.translate = {
+            "MOVE": self.check_move,
+            "PONDER" : self.ponder
+        }
+
+    def main_loop(self):
+        while True:
+            request = self.comms.receive_arbiter_instruction()
+            instruction = self.translate.get(request[0])
+            reply = instruction(request[1])
+            self.comms.send_to_arbiter(request[0],reply)
+
+            
+    def check_move(self):
+        NotImplemented
+
+    def ponder(self):
+            NotImplemented
