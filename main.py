fa8cf8cf0986694cd32e359ba168fed050664567
ThePixelAritst <ondrej@zapadlo.name>
2026-09-23 21:25:10 +0200

    Engine_list managment work

--- main.py
+++ main.py
@@ -1,62 +1,34 @@
-from multiprocessing import Process as mppr
-import multiprocessing.connection as mpcon
-import time
-import random
-
-
-class Engine_Imitator:
-    def __init__(self, engine_number,pipe_conn):
-        self.pipe = pipe_conn
-        translate = {"move":self.turn_call}
-        print(f"Engine {engine_number} initiated")
-
-        while True:
-            request = self.pipe.recv()
-            if request is None:
-                break
-            elif translate.get(request[0]) is not None:
-                return translate.get(request[0])(request[1])
-            else: raise KeyError("Requested function does not exist")
-
-    def turn_call(self,*args):
-        random_timer = random.randrange(1,4)
-        print(f"generated_timer: {random_timer}")
-        time.sleep(random_timer)
-        move = tuple((random.randint(1,10),random.randint(1,10)))
-        print(move)
-        self.pipe.send(move)
-
-class Engine_handler:
-    def __init__(self,engine,engine_numerator):
-        self.identificator = engine_numerator
-        self.arbiter_conn, self.engine_conn = mpcon.Pipe()
-        self.eng = mppr(target=engine,args=(engine_numerator,self.engine_conn))
-        self.eng.start()
-
-    def _receive_data(self,watchdog=2.5):
-        received = mpcon.wait([self.arbiter_conn],watchdog)
-        if self.arbiter_conn in received:
-            return self.arbiter_conn.recv()
-        else: return None
-
-    def move(self,*arguments):
-        self.arbiter_conn.send(("move",arguments))
-        return self._receive_data()
-
-    def _close(self):
-        self.arbiter_conn.send(None)
-        self.eng.join()
-        print(f"Engine {self.identificator} process closed successfully")
-
-    def _terminate(self):
-        self.eng.terminate()
+#import Engine_handler
+import Resources.program_settings as set
+import os
 
 
 
-if __name__ == "__main__":
-    Engine_1 = Engine_handler(Engine_Imitator,1)
+
+class Engine_instance_managment:
+    def __init__(self): #makes a list of all avaliable engines
+        os.chdir(r"D:\Coding adventures\Tic Tac Arbiter\Engines") #set this to set.Engine_folder when you are done with testing
+        
+        self.engine_list = []
+        self._calculate_engine_list()
+
+    def _calculate_engine_list(self):
+        Folder_scan = os.scandir()
+        for engine in Folder_scan:
+                    if engine.is_dir() and "engine" in engine.name.casefold():
+                        self.engine_list.append(engine.path)
+
+    def get_engine_path(self,engine_id):
+        if engine_id >= len(self.engine_list):
+            return None
+        else:
+            return self.engine_list[engine_id]
+
+
+"""if __name__ == "__main__":
+    Engine_1 = Engine_handler.Engine_handler(NotImplemented,1)
     print("stink")
-    Engine_2 = Engine_handler(Engine_Imitator,2)
+    Engine_1 = Engine_handler.Engine_handler(NotImplemented,2)
 
     print("sending test move")
     test_move = Engine_1.move()
@@ -69,3 +41,4 @@ if __name__ == "__main__":
         Engine_1._close()
         Engine_2._close()
         print(test_move)
+"""
